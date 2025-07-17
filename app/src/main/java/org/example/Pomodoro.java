16f373a0bd097be7ecf6b8d4ea52c4fb0b845df0
AlbertoValdezReyes <albertovaldezreyes99@gmail.com>
2025-07-16 18:55:46 -0600

    upgrade config

--- app/src/main/java/org/example/Pomodoro.java
+++ app/src/main/java/org/example/Pomodoro.java
@@ -5,7 +5,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
-import javafx.scene.layout.StackPane;
 import javafx.geometry.Pos;
 import javafx.scene.text.Text;
 
