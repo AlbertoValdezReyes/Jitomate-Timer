150874ff909fa347d9473f901a182ba4b53d2956
AlbertoValdezReyes <albertovaldezreyes99@gmail.com>
2025-07-17 13:54:05 -0600

    upgrade config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+	java
     id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
