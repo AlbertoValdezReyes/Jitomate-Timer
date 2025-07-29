48446d0198e31dac3c8d8ee798b9b08888587730
AlbertoValdezReyes <albertovaldezreyes99@gmail.com>
2025-07-29 15:22:28 -0600

    rewrite of everything

---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+	public static void main(String[] args){
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws IOException {
+		URL fxmlURL = App.class.getResource("primary.fxml");
+
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(fxmlURL);
+		Pane root = (Pane) loader.load();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("Jitomate-Timer");
+		stage.show();
+	}
+}
